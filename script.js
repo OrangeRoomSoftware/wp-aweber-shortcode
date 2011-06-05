adaed2445e48073344475708e97d50d8fba3a334
Phill Kenoyer <phill@kenoyer.com>
2011-06-05 12:21:05 -0700

    Fixed typo

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ var name_placeholder = $('#aweber-signup-form-name').attr('placeholder');
 var email_placeholder = $('#aweber-signup-form-email').attr('placeholder');
 
 if ($('body.ie').length) {
-  $('aweber-signup-form-name').val(name_placeholder).focus(function(){$(this).val('');});
-  $('aweber-signup-form-email').val(email_placeholder).focus(function(){$(this).val('');});
+  $('#aweber-signup-form-name').val(name_placeholder).focus(function(){$(this).val('');});
+  $('#aweber-signup-form-email').val(email_placeholder).focus(function(){$(this).val('');});
 }
