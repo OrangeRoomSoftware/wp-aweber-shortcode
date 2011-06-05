013581f319034d85921a37b013a8fbd4244cbfe0
Phill Kenoyer <phill@kenoyer.com>
2011-06-05 11:40:31 -0700

    Fixed bug in output.

--- aweber-shortcode.php
+++ aweber-shortcode.php
@@ -24,14 +24,14 @@ function aweber_shortcode_handler($atts) {
 $content = <<<END
 <form id="aweber-signup-form" method="post" action="http://www.aweber.com/scripts/addlead.pl">
   <div style="display: none;">
-    <input type="hidden" name="meta_web_form_id" value="<?php echo $form_id; ?>" />
-    <input type="hidden" name="meta_split_id" value="<?php echo $split_id; ?>" />
-    <input type="hidden" name="listname" value="<?php echo $listname; ?>" />
-    <input type="hidden" name="redirect" value="<?php echo $redirect; ?>" />
-    <input type="hidden" name="meta_adtracking" value="<?php echo $adtracking; ?>" />
-    <input type="hidden" name="meta_message" value="<?php echo $message; ?>" />
-    <input type="hidden" name="meta_required" value="<?php echo $required; ?>" />
-    <input type="hidden" name="meta_tooltip" value="<?php echo $tooltip; ?>" />
+    <input type="hidden" name="meta_web_form_id" value="$form_id" />
+    <input type="hidden" name="meta_split_id" value="$split_id" />
+    <input type="hidden" name="listname" value="$listname" />
+    <input type="hidden" name="redirect" value="$redirect" />
+    <input type="hidden" name="meta_adtracking" value="$adtracking" />
+    <input type="hidden" name="meta_message" value="$message" />
+    <input type="hidden" name="meta_required" value="$required" />
+    <input type="hidden" name="meta_tooltip" value="$tooltip" />
   </div>
 
   <img src="/wp-content/plugins/wp-aweber-shortcode/red-arrow.jpeg" style="float:right;" width='40'/>
