6d654a14babcfec22828347bcba5259297672cce
Phill Kenoyer <phill@kenoyer.com>
2011-06-05 12:25:54 -0700

    Document ready.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Placeholders for IE
-var name_placeholder = $('#aweber-signup-form-name').attr('placeholder');
-var email_placeholder = $('#aweber-signup-form-email').attr('placeholder');
+$(document).ready(function() {
+  var name_placeholder = $('#aweber-signup-form-name').attr('placeholder');
+  var email_placeholder = $('#aweber-signup-form-email').attr('placeholder');
 
-if ($('body.ie').length) {
-  $('#aweber-signup-form-name').val(name_placeholder).focus(function(){$(this).val('');});
-  $('#aweber-signup-form-email').val(email_placeholder).focus(function(){$(this).val('');});
-}
+  if ($('body.ie').length) {
+    $('#aweber-signup-form-name').val(name_placeholder).focus(function(){$(this).val('');});
+    $('#aweber-signup-form-email').val(email_placeholder).focus(function(){$(this).val('');});
+  }
+});
